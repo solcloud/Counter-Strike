c393fa3f83be2033a9bc4cd78cc90bc5cfeb7b76
Daniel Maixner <maixner@solidis.cz>
2022-10-03 14:27:26 +0200

    Default 20ms tick rate

--- src/Net/ServerSetting.php
+++ src/Net/ServerSetting.php
@@ -8,7 +8,7 @@ class ServerSetting
 
     public function __construct(
         public readonly int $playersMax,
-        public readonly int $tickMs = 100,
+        public readonly int $tickMs = 20,
         public readonly string $attackerCode = 'acode',
         public readonly string $defenderCode = 'dcode',
         public readonly bool $warmupInstantStart = true,
