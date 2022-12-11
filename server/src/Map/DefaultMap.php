06959d04b86fa6453d41a5aa9fb6f4b2b0f49d2a
Daniel Maixner <solcloud.dev@gmail.com>
2022-12-11 17:25:37 +0100

    Add base y to default map

--- server/src/Map/DefaultMap.php
+++ server/src/Map/DefaultMap.php
@@ -17,6 +17,7 @@ class DefaultMap extends BoxMap
 
     public function __construct()
     {
+        $y = 0;
         $attackers = [];
         $defenders = [];
         $scale = (int)ceil(Setting::playerBoundingRadius() * 1.8);
@@ -24,26 +25,26 @@ class DefaultMap extends BoxMap
         $radiusHalf = Setting::playerBoundingRadius() / 2;
         $boxHeight = Setting::playerHeadHeightCrouch() + 2;
 
-        $this->addBox(new Box(new Point(), 43 * $scale, 5 * Setting::playerHeadHeightStand(), 32 * $scale));
+        $this->addBox(new Box(new Point(0, $y, 0), 43 * $scale, 5 * Setting::playerHeadHeightStand(), 32 * $scale));
         foreach ([5, 13, 21, 29, 37] as $x) {
-            $attackers[] = new Point($x * $scale + $scaleHalf, 0, 4 * $scale - $radiusHalf);
-            $this->addBox(new Box(new Point(($x - 1) * $scale, 0, 5 * $scale), 3 * $scale, $boxHeight, $scale));
-            $this->addBox(new Box(new Point(($x) * $scale, $boxHeight, 5 * $scale), 1 * $scale, $boxHeight, $scale));
+            $attackers[] = new Point($x * $scale + $scaleHalf, $y, 4 * $scale - $radiusHalf);
+            $this->addBox(new Box(new Point(($x - 1) * $scale, $y, 5 * $scale), 3 * $scale, $boxHeight, $scale));
+            $this->addBox(new Box(new Point(($x) * $scale, $y + $boxHeight, 5 * $scale), 1 * $scale, $boxHeight, $scale));
 
-            $defenders[] = new Point($x * $scale + $scaleHalf, 0, 28 * $scale + $radiusHalf);
-            $this->addBox(new Box(new Point(($x - 1) * $scale, 0, 26 * $scale), 3 * $scale, $boxHeight, $scale));
-            $this->addBox(new Box(new Point(($x) * $scale, $boxHeight, 26 * $scale), 1 * $scale, $boxHeight, $scale));
+            $defenders[] = new Point($x * $scale + $scaleHalf, $y, 28 * $scale + $radiusHalf);
+            $this->addBox(new Box(new Point(($x - 1) * $scale, $y, 26 * $scale), 3 * $scale, $boxHeight, $scale));
+            $this->addBox(new Box(new Point(($x) * $scale, $y + $boxHeight, 26 * $scale), 1 * $scale, $boxHeight, $scale));
         }
 
         $stepHeight = 10;
         $stepCount = Setting::playerHeadHeightCrouch() / $stepHeight;
         foreach ([0, 31] as $z) {
-            $ramp1 = new Ramp(new Point(19 * $scale, 0, $z * $scale), new Point2D(1, 0), $stepCount, $scale, true, 12, $stepHeight);
+            $ramp1 = new Ramp(new Point(19 * $scale, $y, $z * $scale), new Point2D(1, 0), $stepCount, $scale, true, 12, $stepHeight);
             foreach ($ramp1->getBoxes() as $box) {
                 $this->addBox($box);
             }
-            $this->addBox(new Box(new Point(21 * $scale, 0, $z * $scale), $scale, Setting::playerHeadHeightCrouch(), $scale));
-            $ramp2 = new Ramp(new Point(24 * $scale - 20, 0, $z * $scale), new Point2D(-1, 0), $stepCount, $scale, true, 12, $stepHeight);
+            $this->addBox(new Box(new Point(21 * $scale, $y, $z * $scale), $scale, Setting::playerHeadHeightCrouch(), $scale));
+            $ramp2 = new Ramp(new Point(24 * $scale - 20, $y, $z * $scale), new Point2D(-1, 0), $stepCount, $scale, true, 12, $stepHeight);
             foreach ($ramp2->getBoxes() as $box) {
                 $this->addBox($box);
             }
@@ -52,9 +53,9 @@ class DefaultMap extends BoxMap
         $this->setAttackersSpawnPositions($attackers);
         $this->setDefendersSpawnPositions($defenders);
 
-        $this->plantArea = new Box(new Point(43 * $scale / 2 - 300, 0, 32 * $scale / 2 - 200), 600, 4, 400);
-        $this->buyArea[0] = new Box((new Point())->addZ(26 * $scale), 43 * $scale, 2 * Setting::playerHeadHeightStand(), 6 * $scale);
-        $this->buyArea[1] = new Box(new Point(), 43 * $scale, 2 * Setting::playerHeadHeightStand(), 6 * $scale);
+        $this->plantArea = new Box(new Point(43 * $scale / 2 - 300, $y, 32 * $scale / 2 - 200), 600, 4, 400);
+        $this->buyArea[0] = new Box((new Point(0, $y, 0))->addZ(26 * $scale), 43 * $scale, 2 * Setting::playerHeadHeightStand(), 6 * $scale);
+        $this->buyArea[1] = new Box(new Point(0, $y, 0), 43 * $scale, 2 * Setting::playerHeadHeightStand(), 6 * $scale);
     }
 
     public function getSpawnRotationDefender(): int
