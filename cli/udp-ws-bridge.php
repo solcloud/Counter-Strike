28b7598e46b7cc0c8947c766bfe9d284c02abf16
Daniel Maixner <solcloud.dev@gmail.com>
2022-12-26 12:28:31 +0100

    WS bridge simplification

--- cli/udp-ws-bridge.php
+++ cli/udp-ws-bridge.php
@@ -1,8 +1,6 @@
 <?php
 
-use Socket\Raw\Exception as UdpException;
 use Socket\Raw\Factory;
-use WebSocket\ConnectionException;
 use WebSocket\Server;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -16,48 +14,23 @@ $addressUdp = "udp://localhost:{$portUdp}";
 $udp = (new Factory())->createClient($addressUdp, 4);
 
 $ws = new Server([
-    'filter' => ['text'],
+    'filter'        => ['text'],
     'fragment_size' => '8192',
-    'port'    => $portWs,
-    'timeout' => 8,
+    'port'          => $portWs,
+    'timeout'       => 8,
 ]);
 
-$logged = false;
-$loginResponse = '';
-
 while (true) {
-    try {
-        $clientMsg = $ws->receive();
-        if (is_string($clientMsg) && $clientMsg !== '') { // if valid ws request
-            if ($clientMsg === 'CLOSE') {
-                break;
-            }
-            if (str_starts_with($clientMsg, 'login ')) { // login request
-                if ($logged) { // client trying reconnect
-                    $ws->text($loginResponse);
-                    while ($udp->selectRead()) {
-                        $udp->read(10241024);
-                    }
-                    continue;
-                } else {
-                    $logged = true;
-                }
-            }
-            $udp->write($clientMsg);
+    $clientMsg = $ws->receive();
+    if ($clientMsg !== '') {
+        if ($clientMsg === 'CLOSE') {
+            break;
         }
-
-        $serverMsg = $udp->read(10241024);
-        if ($loginResponse === '' && $logged) {
-            $loginResponse = $serverMsg;
-        }
-        $ws->send($serverMsg, 'text', false);
-    } catch (ConnectionException $ex) {
-        if ($ex->getCode() !== ConnectionException::TIMED_OUT) {
-            throw $ex;
-        }
-    } catch (UdpException $ex) {
-        break;
+        $udp->write($clientMsg);
     }
+
+    $serverMsg = $udp->read(10241024);
+    $ws->send($serverMsg, 'text', false);
 }
 
 $udp->close();
